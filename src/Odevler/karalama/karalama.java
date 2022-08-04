e8f8b3ca9fc17effe049a8b4656480176973fb74
drej-ali <godguestt.gmail.com>
2022-08-04 13:09:10 +0300

    güncelleme2

--- src/Odevler/karalama/karalama.java
+++ src/Odevler/karalama/karalama.java
@@ -1,6 +1,34 @@
 package Odevler.karalama;
+
+import org.openqa.selenium.By;
+
+import static Utils.BaseStaticDriver.driver;
+
 public class karalama  {
     public static void main(String[] args)  {
+        //animasyon
+        char[] animationChars = new char[]{'|', '/', '-', '\\' };
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.print("Report is being prepared: " + i + "% " + animationChars[i % 4] + "\r");
+
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //iframe bulma
+        int size = driver.findElements(By.tagName("iframe")).size();
+        System.out.println(size);
+        for (int i = 0; i <= size; i++) {
+            driver.switchTo().frame(i);
+            int toplam = driver.findElements(By.xpath("//input[@placeholder='Kart numarası']")).size();
+            System.out.print(toplam);
+            if (toplam==1)break;
+            driver.switchTo (). parentFrame ();
+        }
 
 
 
